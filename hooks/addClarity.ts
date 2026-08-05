2ebfe9d0c01437bd594386794d02510f3c287acf
kraosmortworks <kraosmortworks2228@gmail.com>
2026-08-05 15:57:18 +0530

    moved clarity client side

--- hooks/addClarity.ts
+++ hooks/addClarity.ts
@@ -1,8 +1,12 @@
-import Clarity from '@microsoft/clarity';
+"use client"
 
+import Clarity from '@microsoft/clarity';
+import {useEffect} from 'react';
 // Make sure to add your actual project id instead of "yourProjectId".
 const projectId = "xxkmx1xt6h";
 
 export default function AddClarity() {
-    Clarity.init(projectId);
+    useEffect(() => {
+        Clarity.init(projectId);
+    }, [])
 }
